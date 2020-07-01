6ff2ad20ca4cf33775d6d4976ee5f894ab8faaad
Yixing-Lu <yixinglu97@gmail.com>
2020-06-30 22:30:48 -0700

    first commit

---
+++ Quizzler-iOS13-master/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Yixing Lu on 6/30/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    init(q:String, a:String) {
+        text = q
+        answer = a
+    }
+}
